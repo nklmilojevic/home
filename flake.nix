44448bcb0be80ce75a9a934285480a5042f22fac
Nikola Milojević <nikola@milojevic.me>
2024-06-13 19:53:15 +0200

    feat: switch to nix

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "homeops packaged using poetry2nix";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      in
+      {
+        packages = {
+          home = mkPoetryApplication {
+            projectDir = self;
+            preferWheels = true;
+            };
+          default = self.packages.${system}.home;
+        };
+
+        # Shell for app dependencies.
+        #
+        #     nix develop
+        #
+        # Use this shell for developing your app.
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.home ];
+          buildInputs = [
+            pkgs.age
+            pkgs.fluxcd
+            pkgs.go-task
+            pkgs.kubectl
+            pkgs.kubernetes-helm
+            pkgs.pre-commit
+            pkgs.sops
+          ];
+        };
+
+        # Shell for poetry.
+        #
+        #     nix develop .#poetry
+        #
+        # Use this shell for changes to pyproject.toml and poetry.lock.
+        devShells.poetry = pkgs.mkShell {
+          packages = [ pkgs.poetry ];
+        };
+      });
+}
