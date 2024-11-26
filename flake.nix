a0a3033d7ef34f2b1c50076a8f0378697e8d9690
Nikola Milojević <nikola@milojevic.me>
2024-11-26 20:51:47 +0100

    whoop

--- flake.nix
+++ flake.nix
@@ -8,12 +8,18 @@
       url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    talhelper = {
+      url = "github:budimanjojo/talhelper";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix, talhelper }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+        };
         inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
       in
       {
@@ -22,33 +28,34 @@
             projectDir = self;
             preferWheels = true;
             python = pkgs.python312;
-            };
+          };
           default = self.packages.${system}.home;
         };
 
-        # Shell for app dependencies.
-        #
-        #     nix develop
-        #
-        # Use this shell for developing your app.
         devShells.default = pkgs.mkShell {
           inputsFrom = [ self.packages.${system}.home ];
-          buildInputs = [
-            pkgs.age
-            pkgs.fluxcd
-            pkgs.go-task
-            pkgs.kubectl
-            pkgs.kubernetes-helm
-            pkgs.pre-commit
-            pkgs.sops
+          packages = with pkgs; [
+            age
+            cloudflared
+            fluxcd
+            go-task
+            helmfile
+            kubeconform
+            kubectl
+            kubernetes-helm
+            kustomize
+            minijinja
+            moreutils
+            pre-commit
+            sops
+            stern
+            talosctl
+            yq
+          ] ++ [
+            talhelper.packages.${system}.default
           ];
         };
 
-        # Shell for poetry.
-        #
-        #     nix develop .#poetry
-        #
-        # Use this shell for changes to pyproject.toml and poetry.lock.
         devShells.poetry = pkgs.mkShell {
           packages = [ pkgs.python312 pkgs.poetry ];
         };
